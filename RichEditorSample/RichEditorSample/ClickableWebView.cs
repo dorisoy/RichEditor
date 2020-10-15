416c3cd5252ecd9e3ba473ad46339ca314a54d75
zhenghong.chen@racingandsports.com.au <zhenghong.chen@racingandsports.com.au>
2020-10-15 13:34:53 +0800

    no message

---
+++ RichEditorSample/RichEditorSample/ClickableWebView.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using System.Windows.Input;
+
+namespace RichEditorSample
+{
+    //public class ClickEventArgs : EventArgs
+    //{
+    //    public string Element { get; set; }
+    //}
+
+    //public class ClickableWebView : WebView
+    //{
+    //    public event EventHandler<ClickEventArgs> Clicked;
+
+    //    public static readonly BindableProperty ClickCommandProperty =
+    //        BindableProperty.Create("ClickCommand", typeof(ICommand), typeof(ClickableWebView), null);
+
+    //    public ICommand ClickCommand
+    //    {
+    //        get { return (ICommand)GetValue(ClickCommandProperty); }
+    //        set { SetValue(ClickCommandProperty, value); }
+    //    }
+
+    //    public ClickableWebView()
+    //    {
+    //        LoadFinished += (sender, e) =>
+    //            InjectJavaScript(@"
+    //        document.body.addEventListener('click', function(e) {
+    //            e = e || window.event;
+    //            var target = e.target || e.srcElement;
+    //            Native('invokeClick', 'tag='+target.tagName+' id='+target.id+' name='+target.name);
+    //        }, true /* to ensure we capture it first*/);
+    //    ");
+
+    //        this.RegisterCallback("invokeClick", (string el) => {
+    //            var args = new ClickEventArgs { Element = el };
+
+    //            Clicked?.Invoke(this, args);
+    //            ClickCommand?.Execute(args);
+    //        });
+    //    }
+    //}
+}
---
+++ RichEditorSample/RichEditorSample/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.RichEditor.Shared;
+using Xamarin.Forms;
+
+namespace RichEditorSample
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var mSource = new HtmlWebViewSource
+            {
+                Html = @"<html><body><h1>Test Code</h1><p>The code is working.</p></body></html>"
+            };
+            webview.Source = mSource;
+        }
+
+        private async void Webview_Focused(object sender, FocusEventArgs e)
+        {
+            var texto = await CrossRichEditor.Current.ShowAsync("Começe por aqui");
+            if (texto.IsSave)
+            {
+                var html2 = $"<html><body>{texto.Html}</body></html>";
+                //HtmlLabel.Text = html2;
+                var mSource = new HtmlWebViewSource
+                {
+                    Html = html2
+                };
+                webview.Source = mSource;
+            }
+            webview.Unfocus();
+        }
+    }
+}
